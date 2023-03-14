65b45abc6d06e75a3bec4e7f0dd503aa9d6508d8
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-14 17:00:17 +0100

    Init page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    'class': 'cover-container d-flex w-100 h-100 p-3 mx-auto flex-column'
+  }
 })
 export class AppComponent {
   title = 'biz-landing';
