70d55c279f958012a0e5fe152833b16b6aeb235e
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-10-01 16:17:53 +0200

    Update image and update pacakge and add new enum for languages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,18 @@ import {
   SETTINGS
 } from '@angular/fire/compat/remote-config';
 import {AngularFireModule} from '@angular/fire/compat';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {getRemoteConfig, provideRemoteConfig} from '@angular/fire/remote-config';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient} from "@angular/common/http";
+import {LanguageCodeEnum} from "./enum/language-code.enum";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './asset/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +33,16 @@ import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireRemoteConfigModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideRemoteConfig(() => getRemoteConfig())
+    provideRemoteConfig(() => getRemoteConfig()),
+    TranslateModule.forRoot({
+      useDefaultLang: true,
+      defaultLanguage: LanguageCodeEnum.uk,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     {
---
+++ src/app/enum/language-code.enum.ts
@@ -0,0 +1,21 @@
+export enum LanguageCodeEnum {
+  en = 'en',
+  pl = 'pl',
+  uk = 'uk',
+}
+
+export const LanguageRecord: Record<LanguageCodeEnum, string> = {
+  [LanguageCodeEnum.en]: 'English',
+  [LanguageCodeEnum.uk]: 'Українська',
+  [LanguageCodeEnum.pl]: 'Polski',
+};
+
+export const LANGUAGES: {
+  code: LanguageCodeEnum;
+  name: string;
+}[] = Object.keys(LanguageRecord).map((code) => {
+  return {
+    code,
+    name: LanguageRecord[code as keyof typeof LanguageCodeEnum]
+  } as any;
+});
