fffdaa3d729065e07a953aebab6e9b059aec4a82
Karba <ivan.karbashevskyi@gmail.com>
2024-04-13 10:00:00 +0200

    change firebase config and add google analitics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DEFAULTS, SETTINGS} from '@angular/fire/compat/remote-config';
-import {AngularFireModule} from '@angular/fire/compat';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {environment} from '../environment/environment';
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
@@ -13,6 +12,7 @@ import {isSupportedLanguageCodeEnum, LanguageCodeEnum} from "./enum/language-cod
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {AppService} from "./app.service";
 import {ChangeLanguageComponent} from "./component/change-language/change-language.component";
+import {getAnalytics, provideAnalytics} from "@angular/fire/analytics";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -27,8 +27,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
     TranslateModule.forRoot({
       useDefaultLang: true,
       defaultLanguage: LanguageCodeEnum.uk,
