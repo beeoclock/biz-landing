bade365da66f276690aaf2b1f43a19860a2be99d
Karba <ivan.karbashevskyi@gmail.com>
2024-10-21 00:01:17 +0200

    Fix environment

--- src/environment/environment.production.ts
+++ src/environment/environment.production.ts
@@ -8,7 +8,7 @@ export const environment = {
     appId: "1:188732223564:web:1e02037c639de2f20504e9",
     measurementId: "G-K8P2YKJSPF"
   },
-  production: false,
+  production: true,
   config: {
     language: 'en',
     host: 'https://panel.beeoclock.com',
