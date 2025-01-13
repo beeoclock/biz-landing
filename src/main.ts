2f8099ce4a179da9c3389a967dbefef2d52a740c
Karba <ivan.karbashevskyi@gmail.com>
2025-01-13 20:55:49 +0100

    Fix

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,15 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then((ref) => {
+
+    // Ensure Angular destroys itself on hot reloads.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    window["ngRef"]?.destroy();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    window["ngRef"] = ref;
+
+  })
   .catch((err) => console.error(err));
