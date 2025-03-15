c7eb194e6f13a49a526a5e8b68a57bceb1e5abad
chopyk89 <if040489cvv@gmail.com>
2025-03-15 10:30:38 +0200

    fix language

--- src/app/component/languages/languages.page.ts
+++ src/app/component/languages/languages.page.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-languages',
@@ -7,30 +7,23 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/co
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LanguagesPage {
+  private readonly localeId = inject(LOCALE_ID);
+  public selectedLanguage: string;
 
-  public selectedLanguage: string = 'EN'
-
-  public readonly languageList: {
-    name: string;
-    href: string;
-    code: string;
-  }[] = [
-    {
-      name: 'Dansk',
-      href: '/da',
-      code: 'da'
-    },
-    {
-      name: 'Polski',
-      href: '/pl',
-      code: 'pl'
-    },
-    {
-      name: 'Ukrainian',
-      href: '/uk',
-      code: 'uk'
-    }
+  public readonly languageList: { name: string; href: string; code: string }[] = [
+    { name: 'Dansk', href: '/da', code: 'da' },
+    { name: 'Polski', href: '/pl', code: 'pl' },
+    { name: 'Ukrainian', href: '/uk', code: 'uk' },
+    { name: 'English', href: '/en', code: 'en' }
   ];
+
+  constructor() {
+    this.selectedLanguage = this.languageList.find(lang => lang.code === this.localeId)?.name || 'English';
+  }
+
+  get filteredLanguages() {
+    return this.languageList.filter(lang => lang.name !== this.selectedLanguage);
+  }
 
   onLanguageSelect(language: { name: string; href: string; code: string }) {
     this.selectedLanguage = language.name;
@@ -38,4 +31,3 @@ export class LanguagesPage {
 }
 
 export default LanguagesPage;
-
