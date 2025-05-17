ff3f7b12c9094e1e76ec110bcb3fa974a62ca89c
Andrii Andrusevych <bozonmarketolog@gmail.com>
2025-05-17 18:49:33 +0300

    fix: gTrackingId type

--- environments/develop/environment.ts
+++ environments/develop/environment.ts
@@ -19,6 +19,7 @@ export const environment = {
       panelUrl: 'https://panel.dev.beeoclock.com/identity',
       login: 'demo@beeoclock.com',
       password: 'ItIckBeRSOLDENZYGosicirE'
-    }
+    },
+    gTrackingId: ""
   },
 };
